b06e9d3169c07ff506c59307934f1e6234c89c84
lequangkhai06 <khaidev.com@gmail.com>
2024-12-27 01:03:37 +0700

    modified:   DIEMTHITHPTQG2024.xlsx 	new file:   DIEMTHITHPTQG2024_SORTED_A00.xlsx 	new file:   DIEMTHITHPTQG2024_SORTED_A01.xlsx 	new file:   DIEMTHITHPTQG2024_SORTED_B00.xlsx 	new file:   DIEMTHITHPTQG2024_SORTED_C00.xlsx 	new file:   DIEMTHITHPTQG2024_SORTED_D01.xlsx 	modified:   config.json 	modified:   main.py 	modified:   sort.py

--- main.py
+++ main.py
@@ -2,70 +2,73 @@ import requests
 import pandas as pd
 import json
 
+
+def get_score(key, result):
+    return float(result.get(key, 0) or 0)
+
+
 config_path = 'config.json'
 with open(config_path, "r") as file:
     data = json.load(file)
 
-SBDS = [f'{data['cityCode']}00{id:04}' for id in range(1, data['maxRange'])]
+SBDS = [f"{data['cityCode']}00{id:04}" for id in range(
+    data['minRange'], data['maxRange'])]
 year = data['year']
 all_main_info = []
 file_name = data['fileName']
 
 for SBD in SBDS:
+    print(f">> [ĐANG XỬ LÝ] SBD: {SBD:<12}")
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
-    api_url = 'https://diemthi.vnanet.vn/Home/SearchBySobaodanh?code=' + \
-        str(SBD)+'&nam='+str(year)
+    api_url = f"https://diemthi.vnanet.vn/Home/SearchBySobaodanh?code={SBD}&nam={year}"
     response = requests.get(api_url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        # EXAMPLE DATA
-        # {
-        #     "result": [
-        #         {
-        #             "CityCode": "63",
-        #             "CityArea": null,
-        #             "Code": "63000012",
-        #             "Toan": "4.00",
-        #             "NguVan": "5.25",
-        #             "NgoaiNgu": "2.80",
-        #             "VatLi": "",
-        #             "HoaHoc": "",
-        #             "SinhHoc": "",
-        #             "KHTN": "",
-        #             "DiaLi": "7.25",
-        #             "LichSu": "8.50",
-        #             "GDCD": "8.00",
-        #             "KHXH": "7.92",
-        #             "ResultGroup": "",
-        #             "Result": ""
-        #         }
-        #     ]
-        # }
-        if data['result']:
+
+        if 'result' in data and data['result']:
             result = data['result'][0]
+            code = result['Code']
+
+            toan = get_score('Toan', result)
+            vatli = get_score('VatLi', result)
+            hoahoc = get_score('HoaHoc', result)
+            ngoaingu = get_score('NgoaiNgu', result)
+            nguvan = get_score('NguVan', result)
+            diali = get_score('DiaLi', result)
+            lichsu = get_score('LichSu', result)
+            sinhhoc = get_score('SinhHoc', result)
+            khxh = get_score('KHXH', result)
+            khtn = get_score('KHTN', result)
+            gdcd = get_score('GDCD', result)
+
             main_info = {
-                'SBD': result['Code'],
-                'Toan': result['Toan'],
-                'NguVan': result['NguVan'],
-                'NgoaiNgu': result['NgoaiNgu'],
-                'VatLi': result['VatLi'],
-                'HoaHoc': result['HoaHoc'],
-                'SinhHoc': result['SinhHoc'],
-                'KHTN': result['KHTN'],
-                'DiaLi': result['DiaLi'],
-                'LichSu': result['LichSu'],
-                'GDCD': result['GDCD'],
-                'KHXH': result['KHXH'],
+                'SBD': code,
+                'Toán': toan,
+                'Ngữ Văn': nguvan,
+                'Ngoại Ngữ': ngoaingu,
+                'Vật Lí': vatli,
+                'Hoá Học': hoahoc,
+                'Sinh Học': sinhhoc,
+                'KHTN': khtn,
+                'Địa Lí': diali,
+                'Lịch Sử': lichsu,
+                'GDCD': gdcd,
+                'KHXH': khxh,
+                'A00': toan + vatli + hoahoc,
+                'A01': toan + vatli + ngoaingu,
+                'B00': toan + hoahoc + sinhhoc,
+                'D01': toan + nguvan + ngoaingu,
+                'C00': nguvan + diali + lichsu
             }
             all_main_info.append(main_info)
-            print(">> THÀNH CÔNG SBD: " + str(SBD))
+            print(f">> [THÀNH CÔNG] SBD: {SBD:<12}")
         else:
-            print(">> CÓ LỖI SBD: " + str(SBD))
+            print(f">> [KHÔNG TÌM THẤY] SBD: {SBD:<12}")
     else:
-        print(
-            f'>> Có lỗi xảy ra {response.status_code} SBD {str(SBD)}')
+        print(f">> [LỖI] Mã lỗi {response.status_code:<4} SBD: {SBD:<12}")
         print(response.text)
 
 main_df = pd.DataFrame(all_main_info)
@@ -73,4 +76,4 @@ main_df = pd.DataFrame(all_main_info)
 with pd.ExcelWriter(file_name+'.xlsx') as writer:
     main_df.to_excel(writer, sheet_name='MainInfo', index=False)
 
-print('>> Dữ liệu đã được ghi lại vào file {}.xlsx'.format(file_name))
+print(f">> [HOÀN THÀNH] Dữ liệu đã được ghi lại vào file '{file_name}.xlsx'")
--- sort.py
+++ sort.py
@@ -1,19 +1,22 @@
 import pandas as pd
 
-file_path = 'diem_thi_2024_final.xlsx'
+file_path = 'DIEMTHITHPTQG2024.xlsx'
 
-df = pd.read_excel(file_path, sheet_name='Sheet1')
+df = pd.read_excel(file_path)
 
 df_sorted_A00 = df.sort_values(by='A00', ascending=False)
-df_sorted_A00.to_excel('diem_thi_2024_sorted_A00.xlsx', index=False)
+df_sorted_A00.to_excel('DIEMTHITHPTQG2024_SORTED_A00.xlsx', index=False)
 
 df_sorted_A01 = df.sort_values(by='A01', ascending=False)
-df_sorted_A01.to_excel('diem_thi_2024_sorted_A01.xlsx', index=False)
+df_sorted_A01.to_excel('DIEMTHITHPTQG2024_SORTED_A01.xlsx', index=False)
 
 df_sorted_D01 = df.sort_values(by='D01', ascending=False)
-df_sorted_D01.to_excel('diem_thi_2024_sorted_D01.xlsx', index=False)
+df_sorted_D01.to_excel('DIEMTHITHPTQG2024_SORTED_D01.xlsx', index=False)
 
 df_sorted_C00 = df.sort_values(by='C00', ascending=False)
-df_sorted_C00.to_excel('diem_thi_2024_sorted_C00.xlsx', index=False)
+df_sorted_C00.to_excel('DIEMTHITHPTQG2024_SORTED_C00.xlsx', index=False)
 
-print(">> Hoàn thành sắp xếp và lưu các tệp!")
+df_sorted_B00 = df.sort_values(by='B00', ascending=False)
+df_sorted_B00.to_excel('DIEMTHITHPTQG2024_SORTED_B00.xlsx', index=False)
+
+print(">> ĐÃ HOÀN THÀNH!")
